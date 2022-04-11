188021a6ec10fb8a89244ddf3270d7917e9d3cef
Your Name <you@example.com>
2022-04-11 07:50:26 +0000

    primer commit: Añadiendo proyecto springboot

---
+++ sumador/src/test/java/soy/jesus/sumador/SumadorApplicationTests.java
@@ -0,0 +1,13 @@
+package soy.jesus.sumador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
