28f33187d51846a4aba7ecc71035d032930f06df
Your Name <you@example.com>
2022-04-11 08:49:13 +0000

    tercer commit: Añadiendo Jenkinsfile (etapas compilar + pruebas unitarias)

---
+++ src/main/java/soy/jesus/sumador/SumadorController.java
@@ -0,0 +1,48 @@
+package soy.NOMBRE.sumador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+
+class SumadorController {
+
+     @Autowired
+
+     private Sumador sumador;
+
+
+
+     @RequestMapping("/suma")
+
+     String suma(@RequestParam("a") Integer a, 
+
+                @RequestParam("b") Integer b) {
+
+          return String.valueOf(sumador.suma(a, b));
+
+     }
+
+}
+
+
+
+Actulizamos las dependencias en el archivo build.gradle
+
+
+
+dependencies {
+
+	implementation 'org.springframework.boot:spring-boot-starter-web'
+
+	testImplementation 'org.springframework.boot:spring-boot-starter-test'
+
+}
+
